7f243510b69c653f4d6f5158d3c66f1c3d8cf8f4
Rômulo Fraga <romulo.fraga@vtex.com.br>
2023-06-09 22:42:32 -0300

    Add JWT generation

--- src/services/NSE.Identidade.API/Models/UserViewModels.cs
+++ src/services/NSE.Identidade.API/Models/UserViewModels.cs
@@ -2,29 +2,49 @@
 
 namespace NSE.Identidade.API.Models
 {
-    public class UsuarioRegistro
-    {
-        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
-        [EmailAddress(ErrorMessage = "O campo {0} esta em formato invalido")]
-        public string Email { get; set; }
+  public class UsuarioRegistro
+  {
+    [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+    [EmailAddress(ErrorMessage = "O campo {0} esta em formato invalido")]
+    public string Email { get; set; }
 
-        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
-        [StringLength(100, ErrorMessage = "O campo precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
-        public string Senha { get; set; }
+    [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+    [StringLength(100, ErrorMessage = "O campo precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; }
 
-        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
-        [Compare("Senha", ErrorMessage = "As senhas nao conferem.")]
-        public string SenhaConfirmacao { get; set; }
-    }
+    [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+    [Compare("Senha", ErrorMessage = "As senhas nao conferem.")]
+    public string SenhaConfirmacao { get; set; }
+  }
 
-    public class UsuarioLogin
-    {
-        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
-        [EmailAddress(ErrorMessage = "O campo {0} esta em formato invalido")]
-        public string Email { get; set; }
+  public class UsuarioLogin
+  {
+    [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+    [EmailAddress(ErrorMessage = "O campo {0} esta em formato invalido")]
+    public string Email { get; set; }
 
-        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
-        [StringLength(100, ErrorMessage = "O campo precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
-        public string Senha { get; set; }
-    }
+    [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+    [StringLength(100, ErrorMessage = "O campo precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    public string Senha { get; set; }
+  }
+
+  public class UsuarioRespostaLogin
+  {
+    public string AccessToken { get; set; }
+    public double ExpiresIn { get; set; }
+    public UsuarioToken UsuarioToken { get; set; }
+  }
+
+  public class UsuarioToken
+  {
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<UsuarioClaim> Claims { get; set; }
+  }
+
+  public class UsuarioClaim
+  {
+    public string Value { get; set; }
+    public string Type { get; set; }
+  }
 }
