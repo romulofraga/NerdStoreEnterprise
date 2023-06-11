1aa28d25b31c00114ba6a4aa8ea29075facb1f56
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-06-11 18:27:16 -0300

    Custom Response

---
+++ src/services/NSE.Identidade.API/Controllers/MainController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        protected ICollection<string> Erros = new List<string>();
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                {"Mensagens", Erros.ToArray() }
+            }));
+
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelstate)
+        {
+            var errors = modelstate.Values.SelectMany(error => error.Errors);
+            foreach (var error in errors)
+            {
+                AdicionarErroProcessamento(error.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !Erros.Any();
+        }
+
+        protected void AdicionarErroProcessamento(string erro)
+        {
+            Erros.Add(erro);
+        }
+
+        protected void LimparerrosProcessamento()
+        {
+            Erros.Clear();
+        }
+    }
+}
