f60ba719402b1538b36425a52defbcca0394762f
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-06-21 23:18:50 -0300

    Setup Database Context

---
+++ src/building blocks/NSE.Core/Data/IUnityOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace NSE.Core.Data
+{
+    public interface IUnityOfWork
+    {
+        Task<bool> Commit();
+    }
+}
--- src/services/NSE.Catalogo.API/Program.cs
+++ src/services/NSE.Catalogo.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Data;
+using NSE.Catalogo.API.Data.Repository;
+using NSE.Catalogo.API.Models;
+
 namespace NSE.Catalogo.API
 {
     public class Program
@@ -7,6 +12,14 @@ namespace NSE.Catalogo.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<CatalogoContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            builder.Services.AddScoped<CatalogoContext>();
+
+            builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
