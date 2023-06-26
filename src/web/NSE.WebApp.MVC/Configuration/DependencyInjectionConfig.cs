6ef8e249e74ad6ff9c9d07e9db8dc3f66fa8541e
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-06-25 22:13:32 -0300

    Add Poly

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,8 @@
 ﻿using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -12,9 +14,27 @@ namespace NSE.WebApp.MVC.Configuration
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
+            var retryWaitPolicy = HttpPolicyExtensions.HandleTransientHttpError()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10),
+                },
+                (outcome, timespan, retryCount, context) =>
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Tentando pela {retryCount} vez!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                });
+
             services
                 .AddHttpClient<ICatalogoService, CatalogoService>()
-                .AddHttpMessageHandler<HttpClientAuthorizationDelegateHandler>();
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegateHandler>()
+                //.AddTransientHttpErrorPolicy(
+                //    policy => policy.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600))
+                //);
+                .AddPolicyHandler(retryWaitPolicy);
 
             services
                 .AddHttpClient("Refit", options =>
