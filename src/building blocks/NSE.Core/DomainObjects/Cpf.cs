f1066f2a8552cca8dca13c086b8d2e45eb9ed58f
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-06-29 19:15:55 -0300

    Mapeando Entidades de dominio e objetos de valor

---
+++ src/building blocks/NSE.Core/DomainObjects/Cpf.cs
@@ -0,0 +1,60 @@
+﻿namespace NSE.Core.DomainObjects
+{
+    public class Cpf
+    {
+        public const int CpfMaxLength = 11;
+        public string Numero { get; private set; }
+
+        protected Cpf() { }
+
+        public Cpf(string numero)
+        {
+            if (!Validar(numero)) throw new DomainException("CPF Invalido");
+            Numero = numero;
+        }
+
+        public static bool Validar(string cpf)
+        {
+            // Remove any non-digit characters from the input
+            string digits = new(cpf.Where(char.IsDigit).ToArray());
+
+            // Check if the CPF has the correct length
+            if (digits.Length != 11)
+            {
+                return false;
+            }
+
+            // Check if all digits are the same (e.g., 00000000000)
+            bool allSameDigits = digits.Distinct().Count() == 1;
+            if (allSameDigits)
+            {
+                return false;
+            }
+
+            // Calculate the verification digits
+            int[] factors = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] cpfDigits = digits.Take(9).Select(c => int.Parse(c.ToString())).ToArray();
+            int[] firstVerifier = CalculateVerifiers(cpfDigits, factors);
+            int[] secondVerifier = CalculateVerifiers(cpfDigits.Concat(firstVerifier).ToArray(), factors);
+
+            // Compare the calculated verifiers with the provided ones
+            return digits.Substring(9, 2) == $"{firstVerifier[0]}{secondVerifier[0]}";
+        }
+
+        private static int[] CalculateVerifiers(int[] digits, int[] factors)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                sum += digits[i] * factors[i];
+            }
+
+            int remainder = sum % 11;
+            int verifier = remainder < 2 ? 0 : 11 - remainder;
+
+            return new[] { verifier };
+        }
+
+    }
+}
---
+++ src/building blocks/NSE.Core/DomainObjects/Email.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Email
+    {
+        public const int EmailMaxLength = 245;
+        public const int EmailMinLength = 5;
+
+        public string Endereco { get; set; }
+
+        protected Email()
+        {
+
+        }
+
+        public Email(string endereco)
+        {
+            if (!Validar(endereco)) throw new DomainException("E-mail invalido");
+            Endereco = endereco;
+        }
+
+
+        public static bool Validar(string email)
+        {
+            // Regular expression pattern for email validation
+            string pattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+
+            // Check if the email matches the pattern
+            Match match = Regex.Match(email, pattern);
+
+            return match.Success;
+        }
+
+    }
+}
