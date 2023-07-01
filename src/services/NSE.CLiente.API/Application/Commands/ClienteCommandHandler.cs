8924241edbbb961833a516dd9eb6b0fb7236b5ae
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-07-01 12:00:19 -0300

    Create ClienteCommands

---
+++ src/services/NSE.CLiente.API/Application/Commands/ClienteCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace NSE.Clientes.API.Application.Commands
+{
+    public class ClienteCommandHandler
+    {
+        public void Manipular(RegistrarClienteCommand message)
+        {
+            // validar command
+
+            // persistir na base
+        }
+    }
+}
