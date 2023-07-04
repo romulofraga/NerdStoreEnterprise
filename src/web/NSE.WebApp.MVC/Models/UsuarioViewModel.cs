d68da14613e0e61a847f644a9f1706225f59eae6
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-07-03 22:23:33 -0300

    Add Registration data and validation in MVC

--- src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
@@ -1,11 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using NSE.WebApp.MVC.Extensions;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace NSE.WebApp.MVC.Models
 {
     public class UsuarioRegistro
     {
         [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [DisplayName("Nome Completo")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
+        [DisplayName("CPF")]
+        [Cpf]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} e obrigatorio")]
         [EmailAddress(ErrorMessage = "O campo {0} esta em formato invalido")]
+        [DisplayName("E-mail")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O campo {0} e obrigatorio")]
@@ -14,6 +26,7 @@ namespace NSE.WebApp.MVC.Models
 
         [Required(ErrorMessage = "O campo {0} e obrigatorio")]
         [Compare("Senha", ErrorMessage = "As senhas nao conferem.")]
+        [DisplayName("Confirme sua senha")]
         public string SenhaConfirmacao { get; set; }
     }
 
