22cbb2e4c434419a77133d537682e6c0664afc37
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-07-30 22:55:56 -0300

    setup Pedido API Project

---
+++ src/services/NSE.Pedidos/NSE.Pedido.API/Configurarion/DependencyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿namespace NSE.Pedidos.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            //services.AddScoped<IMediatorHandler, MediatorHandler>();
+            //services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
+
+            //services.AddScoped<INotificationHandler<ClienteRegistradoEvent>, ClienteEventHandler>();
+
+            //services.AddScoped<IClienteRepository, ClienteRepository>();
+            //services.AddScoped<ClientesContext>();
+
+            return services;
+        }
+    }
+}
