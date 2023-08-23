3d2de979c5fa685d4a3e2de5bdc87458fa13218f
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-08-23 11:44:01 -0300

    addd dependencies

--- src/services/NSE.Pedidos/NSE.Pedido.API/Configurarion/DependencyInjectionConfig.cs
+++ src/services/NSE.Pedidos/NSE.Pedido.API/Configurarion/DependencyInjectionConfig.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 using NSE.Core.Mediator;
+using NSE.Pedidos.API.Application.Commands;
+using NSE.Pedidos.API.Application.Events;
 using NSE.Pedidos.API.Application.Queries;
+using NSE.Pedidos.Domain.Pedidos;
 using NSE.Pedidos.Domain.Vouchers;
 using NSE.Pedidos.Infra.Data;
 using NSE.Pedidos.Infra.Data.Repository;
@@ -15,17 +20,21 @@ namespace NSE.Pedidos.API.Configuration
             //API
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspnetUser, AspNetUser>();
+            //Commands
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+            //Events
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
             //Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
             //Data
             services.AddDbContext<PedidosContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddScoped<IVoucherRepository, VoucherRepository>();
-
-
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
 
             return services;
         }
