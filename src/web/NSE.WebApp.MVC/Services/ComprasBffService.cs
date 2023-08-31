87f13dc867baa74c19b9cc85df235ebbeff819a4
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-08-31 18:58:45 -0300

    Pedido controller mvc

--- src/web/NSE.WebApp.MVC/Services/ComprasBffService.cs
+++ src/web/NSE.WebApp.MVC/Services/ComprasBffService.cs
@@ -14,6 +14,11 @@ namespace NSE.WebApp.MVC.Services
         Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoViewModel produto);
         Task<ResponseResult> RemoverItemCarrinho(Guid produtoId);
         Task<ResponseResult> AplicarVoucherCarrinho(string voucher);
+        Task<ResponseResult> FinalizarPedido(PedidoTransacaoViewModel pedidoTransacao);
+        Task<PedidoViewModel> ObterUltimoPedido();
+        Task<IEnumerable<PedidoViewModel>> ObterListaPorClienteId();
+        PedidoTransacaoViewModel MapearParaPedido(CarrinhoViewModel carrinho, EnderecoViewModel endereco);
+
     }
 
     public class ComprasBffService : Service, IComprasBffService
@@ -29,6 +34,7 @@ namespace NSE.WebApp.MVC.Services
         public async Task<CarrinhoViewModel> ObterCarrinho()
         {
             var response = await _httpClient.GetAsync("compras/carrinho/");
+
             TratarErrosResponse(response);
 
             return await DeserializarObjetoResponse<CarrinhoViewModel>(response);
@@ -36,42 +42,40 @@ namespace NSE.WebApp.MVC.Services
 
         public async Task<int> ObterQuantidadeCarrinho()
         {
-            var response = await _httpClient.GetAsync("compras/carrinho-quantidade/");
+            var response = await _httpClient.GetAsync("/compras/carrinho-quantidade/");
+
             TratarErrosResponse(response);
 
             return await DeserializarObjetoResponse<int>(response);
         }
 
-        public async Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoViewModel carrinho)
+        public async Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoViewModel produto)
         {
-            var itemContent = ObterConteudo(carrinho);
+            var itemContent = ObterConteudo(produto);
 
-            var response = await _httpClient.PostAsync("compras/carrinho/items", itemContent);
+            var response = await _httpClient.PostAsync("/compras/carrinho/items/", itemContent);
 
-            if (!TratarErrosResponse(response))
-                return await DeserializarObjetoResponse<ResponseResult>(response);
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
 
             return RetornoOK();
         }
 
-        public async Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoViewModel item)
+        public async Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoViewModel produto)
         {
-            var itemContent = ObterConteudo(item);
+            var itemContent = ObterConteudo(produto);
 
-            var response = await _httpClient.PutAsync($"compras/carrinho/items/{produtoId}", itemContent);
+            var response = await _httpClient.PutAsync($"/compras/carrinho/items/{produto.ProdutoId}", itemContent);
 
-            if (!TratarErrosResponse(response))
-                return await DeserializarObjetoResponse<ResponseResult>(response);
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
 
             return RetornoOK();
         }
 
         public async Task<ResponseResult> RemoverItemCarrinho(Guid produtoId)
         {
-            var response = await _httpClient.DeleteAsync($"compras/carrinho/items/{produtoId}");
+            var response = await _httpClient.DeleteAsync($"/compras/carrinho/items/{produtoId}");
 
-            if (!TratarErrosResponse(response))
-                return await DeserializarObjetoResponse<ResponseResult>(response);
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
 
             return RetornoOK();
         }
@@ -80,11 +84,68 @@ namespace NSE.WebApp.MVC.Services
         {
             var itemContent = ObterConteudo(voucher);
 
-            var response = await _httpClient.PostAsync("/compras/carrinho/aplicar-voucher", itemContent);
+            var response = await _httpClient.PostAsync("/compras/carrinho/aplicar-voucher/", itemContent);
 
             if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
 
             return RetornoOK();
+        }
+
+        public async Task<ResponseResult> FinalizarPedido(PedidoTransacaoViewModel pedidoTransacao)
+        {
+            var pedidoContent = ObterConteudo(pedidoTransacao);
+
+            var response = await _httpClient.PostAsync("/compras/pedido/", pedidoContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOK();
+        }
+
+        public async Task<PedidoViewModel> ObterUltimoPedido()
+        {
+            var response = await _httpClient.GetAsync("/compras/pedido/ultimo/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<PedidoViewModel>(response);
+        }
+
+        public async Task<IEnumerable<PedidoViewModel>> ObterListaPorClienteId()
+        {
+            var response = await _httpClient.GetAsync("/compras/pedido/lista-cliente/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<PedidoViewModel>>(response);
+        }
+
+        public PedidoTransacaoViewModel MapearParaPedido(CarrinhoViewModel carrinho, EnderecoViewModel endereco)
+        {
+            var pedido = new PedidoTransacaoViewModel
+            {
+                ValorTotal = carrinho.ValorTotal,
+                Itens = carrinho.Itens,
+                Desconto = carrinho.Desconto,
+                VoucherUtilizado = carrinho.VoucherUtilizado,
+                VoucherCodigo = carrinho.Voucher?.Codigo
+            };
+
+            if (endereco != null)
+            {
+                pedido.Endereco = new EnderecoViewModel
+                {
+                    Logradouro = endereco.Logradouro,
+                    Numero = endereco.Numero,
+                    Bairro = endereco.Bairro,
+                    Cep = endereco.Cep,
+                    Complemento = endereco.Complemento,
+                    Cidade = endereco.Cidade,
+                    Estado = endereco.Estado
+                };
+            }
+
+            return pedido;
         }
     }
 }
