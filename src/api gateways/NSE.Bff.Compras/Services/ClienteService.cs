7855215d588a8528bf4120c400c1f3edc1678ef5
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-08-31 22:03:14 -0300

    add cliente service

---
+++ src/api gateways/NSE.Bff.Compras/Services/ClienteService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using NSE.Bff.Compras.Extensions;
+using NSE.Bff.Compras.Models;
+using NSE.Bff.Compras.Services;
+using System.Net;
+
+namespace NSE.BFF.Compras.Services
+{
+    public interface IClienteService
+    {
+        Task<EnderecoDTO> ObterEndereco();
+    }
+
+    public class ClienteService : Service, IClienteService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ClienteService(HttpClient httpClient, IOptions<AppServicesSettings> settings)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(settings.Value.ClienteUrl);
+        }
+
+        public async Task<EnderecoDTO> ObterEndereco()
+        {
+            var response = await _httpClient.GetAsync("/cliente/endereco/");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<EnderecoDTO>(response);
+        }
+    }
+}
