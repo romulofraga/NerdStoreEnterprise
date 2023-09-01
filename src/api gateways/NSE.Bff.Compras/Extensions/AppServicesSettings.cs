e207e40628d250b337b4c0aa16bc51075e72b492
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-08-31 22:02:47 -0300

    add clientURL

--- src/api gateways/NSE.Bff.Compras/Extensions/AppServicesSettings.cs
+++ src/api gateways/NSE.Bff.Compras/Extensions/AppServicesSettings.cs
@@ -3,7 +3,7 @@
     public class AppServicesSettings
     {
         public string CatalogoUrl { get; set; }
-        public string ClientesUrl { get; set; }
+        public string ClienteUrl { get; set; }
         public string CarrinhoUrl { get; set; }
         public string PedidoUrl { get; set; }
     }
