b19fd8786cb4a9f4959940260d1441395eda3c88
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2023-09-21 23:36:39 -0300

    create PedidoIniciadoIntegrationEvent

--- src/building blocks/NSE.Core/Messages/Integration/IntegrationEvent.cs
+++ src/building blocks/NSE.Core/Messages/Integration/IntegrationEvent.cs
@@ -4,20 +4,4 @@
     {
 
     }
-
-    public class UsuarioRegistradoIntegradoEvent : IntegrationEvent
-    {
-        public Guid Id { get; private set; }
-        public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public string Cpf { get; private set; }
-
-        public UsuarioRegistradoIntegradoEvent(Guid id, string nome, string email, string cpf)
-        {
-            Id = id;
-            Nome = nome;
-            Email = email;
-            Cpf = cpf;
-        }
-    }
 }
--- src/services/NSE.Pedidos/NSE.Pedido.API/Application/Commands/PedidoCommandHandler.cs
+++ src/services/NSE.Pedidos/NSE.Pedido.API/Application/Commands/PedidoCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using FluentValidation.Results;
 using MediatR;
 using NSE.Core.Messages;
+using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
 using NSE.Pedidos.API.Application.DTO;
 using NSE.Pedidos.API.Application.Events;
 using NSE.Pedidos.Domain.Pedidos;
@@ -13,11 +15,13 @@ namespace NSE.Pedidos.API.Application.Commands
     {
         private readonly IVoucherRepository _voucherRepository;
         private readonly IPedidoRepository _pedidoRepository;
+        private readonly IMessageBus _bus;
 
-        public PedidoCommandHandler(IVoucherRepository voucherRepository, IPedidoRepository pedidoRepository)
+        public PedidoCommandHandler(IVoucherRepository voucherRepository, IPedidoRepository pedidoRepository, IMessageBus bus)
         {
             _voucherRepository = voucherRepository;
             _pedidoRepository = pedidoRepository;
+            _bus = bus;
         }
 
         public async Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
@@ -35,7 +39,7 @@ namespace NSE.Pedidos.API.Application.Commands
             if (!ValidarPedido(pedido)) return ValidationResult;
 
             //Processar pagamento
-            if (!ProcessarPagamento(pedido)) return ValidationResult;
+            if (!await ProcessarPagamento(pedido, message)) return ValidationResult;
 
             //Se Pagamento ok
             pedido.AutorizarPedido();
@@ -48,9 +52,31 @@ namespace NSE.Pedidos.API.Application.Commands
             return await PersistirDados(_pedidoRepository.UnityOfWork);
         }
 
-        private static bool ProcessarPagamento(Pedido pedido)
+        private async Task<bool> ProcessarPagamento(Pedido pedido, AdicionarPedidoCommand message)
         {
-            return true;
+            var pedidoIniciado = new PedidoIniciadoIntegrationEvent
+            {
+                PedidoId = pedido.Id,
+                ClienteId = pedido.ClienteId,
+                Valor = pedido.ValorTotal,
+                TipoPagamento = 1,
+                NomeCartao = message.NomeCartao,
+                NumeroCartao = message.NumeroCartao,
+                MesAnoVencimento = message.ExpiracaoCartao,
+                CVV = message.CVVCartao
+            };
+
+            var result = await _bus.RequestAsync<PedidoIniciadoIntegrationEvent, ResponseMessage>(pedidoIniciado);
+
+            if (!result.ValidationResult.IsValid)
+            {
+                foreach (var erro in result.ValidationResult.Errors)
+                {
+                    AdicionarErro(erro.ErrorMessage);
+                }
+            }
+
+            return result.ValidationResult.IsValid;
         }
 
         private bool ValidarPedido(Pedido pedido)
